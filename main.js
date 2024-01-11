e52bf9616679b8d3b98c3d88ab4956b866349de3
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-11 16:30:43 +0100

    fix:migliorato il codice

--- main.js
+++ main.js
@@ -5,39 +5,25 @@ for(let i = 1; i <= 100; i++) {
     let x;
     if( i % 15 == 0){
         x = "FizzBuzz";
-        boxContainer.innerHTML += `
-        <div class="col-1 my-3 ">
-            <div class="card text-bg-danger">${x}</div>
-        </div>   
-        `
         
     }else if( i % 5 == 0){
         x = "Buzz";
-        boxContainer.innerHTML += `
-        <div class="col-1 my-3 ">
-            <div class="card text-bg-warning">${x}</div>
-        </div>   
-        `
         
     }else if(i % 3 == 0){
         x = "Fizz";
-        boxContainer.innerHTML += `
-        <div class="col-1 my-3 ">
-            <div class="card text-bg-success">${x}</div>
-        </div>   
-        `
         
-      
     }else{
-        x =i
-        boxContainer.innerHTML += `
-            <div class="col-1 my-3 ">
-                <div class="card">${x}</div>
-            </div>   
-        `
-      
+       
+       x = i;
     }
-
+ const boxElement =`
+        
+             <div class="col-1 my-3 " >
+                <div class="card ${x}">${x}</div>
+             </div>
+       
+`
+ boxContainer.innerHTML += boxElement ;
     
     
 };
