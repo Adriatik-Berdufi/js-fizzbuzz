c96e07617f483202cddd902d275aa0d09410f2b2
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-11 14:46:45 +0100

    feat:add stamapa risultati a schermo

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-for(let i = 1; i <= 100; i++){
-    if( i % 15 ==0){
+
+const boxContainer = document.getElementById ('container');
+
+for(let i = 1; i <= 100; i++) {
+    let x;
+    if( i % 15 == 0){
+        x = "FizzBuzz";
         console.log("FizzBuzz");
     }else if( i % 5 == 0){
+        x = "Buzz";
         console.log("Buzz");
     }else if(i % 3 == 0){
+        x = "Fizz";
         console.log("Fizz");
     }else{
+        x =i
         console.log(i);
     }
 
+
+    boxContainer.innerHTML += `
+        <div>
+            <h4> ${x} <h4/>
+        <div/>
+    `
 };
